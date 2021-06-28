e0f195834efb59b5f9dfd02239c784c10327fde8
Sharaar <walidsharar@gmail.com>
2021-06-28 18:59:25 +0200

    Saving data

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website=website_entry.get()
+    email=email_entry.get()
+    password=password_entry.get()
+    with open("data.txt","a") as data_file:
+        data_file.write(f"{website} | {email}  | {password}\n")
+        website_entry.delete(0,END)
+        password_entry.delete(0,END)
+
+
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -26,8 +38,10 @@ password_label.grid(row=3,column=0)
 
 website_entry = Entry(width=35)
 website_entry.grid(row=1,column=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2,column=1,columnspan=2)
+email_entry.insert(0,"abc@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3,column=1)
 
@@ -35,7 +49,7 @@ password_entry.grid(row=3,column=1)
 
 generat_password_button= Button(text="Generate Password")
 generat_password_button.grid(row=3,column=2)
-add_button =Button(text="Add", width=36)
+add_button =Button(text="Add", width=36,command=save)
 add_button.grid(row=4,column=1,columnspan=2)
 
 window.mainloop()
